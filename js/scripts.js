9d1bee51cb55108fe3f51b71185c2c7b1af07b8c
Thomas Glenn <thomasgrantglenn@mac.com>
2020-05-06 15:16:14 -0600

    get condition to display

--- js/scripts.js
+++ js/scripts.js
@@ -9,42 +9,42 @@ var add = function(age, sit, fail, life) {
 $(document).ready(function() {
   $("form#profile").submit(function(event) {
     event.preventDefault();
-    var name1 = $("input#firstName").val();
-    var name2 = $("input#lastName").val();
-    var address = $("input#address").val();
-    var phone = $("input#phone").val();
+    // var name1 = $("input#firstName").val();
+    // var name2 = $("input#lastName").val();
+    // var address = $("input#address").val();
+    // var phone = $("input#phone").val();
    
-    $(".firstName").text(name1);
-    $(".lastName").text(name2);
-    $(".address").text(address);
-    $(".phone").text(phone);
+    // $(".firstName").text(name1);
+    // $(".lastName").text(name2);
+    // $(".address").text(address);
+    // $(".phone").text(phone);
 
-    var info = display(name1, name2, address, phone);
+    // var info = display(name1, name2, address, phone);
   
   // this code will display the results of the quiz:
   
-    // var age = parseInt($("input:radio[name=age]:checked").val());
-    // var sit = parseInt($("input:radio[name=sit]:checked").val());
-    // var fail = parseInt($("input:radio[name=fail]:checked").val());
-    // var life = parseInt($("input:radio[name=life]:checked").val());
+    var age = parseInt($("input:radio[name=age]:checked").val());
+    var sit = parseInt($("input:radio[name=sit]:checked").val());
+    var fail = parseInt($("input:radio[name=fail]:checked").val());
+    var life = parseInt($("input:radio[name=life]:checked").val());
 
   
-    // var language = add(age, sit, fail, life);
+    var language = add(age, sit, fail, life);
 
    
-    // if (language === 4) {
-    //   var result = alert("C#");
-    // } else if (language === 3) {
-    //   var result = alert("JavaScript");
-    // } else if (language === 2) {
-    //   var result = alert("Python");
-    // } else if (language <= 1) {
-    //   var result = alert("Swift");
-    // }
+    if (language === 4) {
+      var result = "C#";
+    } else if (language === 3) {
+      var result = "JavaScript";
+    } else if (language === 2) {
+      var result = "Python";
+    } else if (language <= 1) {
+      var result = "Swift";
+    }
     
     // $(".result").text(result);
 
-    $("#output").text(info);
+    $("#output").text(result);
   });
 });
 
